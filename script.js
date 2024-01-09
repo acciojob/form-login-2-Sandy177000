7c5d363e84ba8e06b19522b4795761bd83c3c6b9
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-01-09 22:11:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function func() {
+	let fname = document.forms["form1"]["fname"].value;
+	let lname = document.forms["form1"]["lname"].value;
+	let pname = document.forms["form1"]["pname"].value;
+	let ename = document.forms["form1"]["ename"].value;
+
+	alert("First Name": fname + '\n'
+		  + "Last Name": lname + '\n' +
+		 "Phone Number": pname + '\n'
+		  + "Email ID": ename)
+}
